b1bfb09b355a39f8c576feb168ed9e7ae55c6862
lengh <lengh@TRICK>
2025-01-20 17:07:17 +0200

    Add project files.

---
+++ set1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace set1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double x = -b / a;
+            Console.WriteLine(x);
+        }
+    }
+}
